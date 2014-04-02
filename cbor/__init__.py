71829abd8cab8de7a68b2de9928492a738e4f5a0
Brian Olson <brian.olson@gmail.com>
2014-04-02 14:49:45 -0400

    actually export load() and dump()

--- cbor/__init__.py
+++ cbor/__init__.py
@@ -2,10 +2,10 @@
 
 try:
     # try C library _cbor.so
-    from ._cbor import loads, dumps
+    from ._cbor import loads, dumps, load, dump
 except:
     # fall back to 100% python implementation
-    from .cbor import loads, dumps
+    from .cbor import loads, dumps, load, dump
 
 
-__all__ = ['loads', 'dumps']
+__all__ = ['loads', 'dumps', 'load', 'dump']
