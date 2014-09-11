104c7b6f0a0707b9aca7a49f3346217889c69c85
Brian Olson <brian.olson@gmail.com>
2014-09-11 12:13:37 -0400

    switch license to Apache2, declare it to be at least Beta quality. It is used now in a few places. No bugs for 6 months and I will declare 1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 #! /usr/bin/env python
+# Copyright 2014 Brian Olson
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
 
 setup(
     name='cbor',
-    version='0.1.10',
+    version='0.1.11',
     description='RFC 7049 - Concise Binary Object Representation',
     long_description="""
 An implementation of RFC 7049 - Concise Binary Object Representation (CBOR).
@@ -29,11 +42,11 @@ This library includes a C implementation which runs 3-5 times faster than the Py
             headers=['c/cbor.h'],
         )
     ],
-    license='AGPL',
+    license='Apache',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+	'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
