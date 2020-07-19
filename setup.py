d6f113321bcf5dcb97fc11ddfeb56a70933fdd31
Brian Dolbec <dolsen@gentoo.org>
2020-07-19 16:30:23 -0700

    setup.py: Add tests to packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 # Copyright 2014 Brian Olson
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -75,7 +75,7 @@ This library includes a C implementation which runs 3-5 times faster than the Py
     author='Brian Olson',
     author_email='bolson@bolson.org',
     url='https://github.com/brianolson/cbor_py',
-    packages=['cbor'],
+    packages=['cbor', 'cbor/tests'],
     package_dir={'cbor':'cbor'},
     ext_modules=[
         Extension(
